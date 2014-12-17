7fd7ff4bf9b02c465149c020fb4c8dc6d6e53201
Yan, Andrew N <ayan@usgs.gov>
2014-12-17 14:45:11 -0600

    added working functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache and return the inverse of a matrix
 
-## Write a short comment describing this function
+## Return a list of three functions to: get a matrix, cache its inverse, and get its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  get_matrix <- function() {
+    x
+  }
+  set_inverse <- function(matrix_inverse) {
+    inverse <<- matrix_inverse
+  }
+  get_inverse <- function() {
+    inverse
+  }
+  cache_functions <- list(get_matrix = get_matrix,
+                          set_inverse = set_inverse,
+                          get_inverse = get_inverse
+  )
 }
 
 
-## Write a short comment describing this function
+## Return the inverse of a matrix from the cache,
+## if it is null, calculate the inverse, cache it, 
+## and return the value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inverse <- x$get_inverse()
+  if(!is.null(inverse)) {
+    return(inverse)
+  }
+  matrix <- x$get_matrix()
+  matrix_inverse <- solve(matrix, ...)
+  x$set_inverse(matrix_inverse)
+  matrix_inverse
 }
